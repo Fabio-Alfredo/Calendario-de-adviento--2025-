06de2cf2909b7267cfb1e13d18ec42c463c52464
Fabio-Alfredo <00210121@uca.edu.sv>
2025-12-30 17:07:52 -0600

    :sparkles: feat: add solution by challenge fourteen

---
+++ reto-14/main.js
@@ -0,0 +1,42 @@
+/**
+ * @param {object} workshop - A representation of the workshop
+ * @param {string|number|boolean} gift - The gift to find
+ * @returns {number[]} The path to the gift
+ */
+function findGiftPath(workshop, gift) {
+  function search(obj, path) {
+    for (const key in obj) {
+      const value = obj[key]
+
+      if (value === gift) {
+        return [...path, key]
+      }
+
+      if (typeof value === 'object' && value !== null) {
+        const result = search(value, [...path, key])
+        if (result.length) return result
+      }
+    }
+
+    return []
+  }
+
+  return search(workshop, [])
+}
+
+const workshop = {
+  storage: {
+    shelf: {
+      box1: 'train',
+      box2: 'switch'
+    },
+    box: 'car'
+  },
+  gift: 'doll'
+}
+
+console.log(findGiftPath(workshop, 'train'))
+console.log(findGiftPath(workshop, 'switch'))
+console.log(findGiftPath(workshop, 'car'))
+console.log(findGiftPath(workshop, 'doll'))
+console.log(findGiftPath(workshop, 'plane'))
